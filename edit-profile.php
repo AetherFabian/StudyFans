cbee944f969abb850804daf4c8566a209ebd28b0
Joshua Avilés <80539008+JoshuaAv07@users.noreply.github.com>
2021-11-24 23:10:45 -0700

    Falla el update 2.0

---
+++ edit-profile.php
@@ -0,0 +1,97 @@
+<?php
+    require 'session.php';
+?>
+<!DOCTYPE html>
+<html class="text-center" lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Editar perfil</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/Profile-Edit-Form.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+
+<body>
+    <?php if(!empty($id)): ?>
+
+    <div class="container profile profile-view" id="profile">
+        
+        <div class="row">
+            <div class="col-md-12 alert-col relative">
+                <div class="alert alert-info alert-dismissible absolue center" role="alert"><button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button><span>El perfil fue guardado exitosamente.</span></div>
+            </div>
+        </div>
+
+     <form action="#" method="POST" enctype="multipart/form-data">
+        <div class="row profile-row">
+            
+             <div class="col-md-4 relative">
+                
+                <div class="avatar">
+                     <div class="avatar-bg center"></div>
+                 </div><label class="form-label"><h2><?= $id['name_user']; ?></h2></label>
+                 
+                <div class="form-group mb-3"><label class="form-label">Descripción de perfil</label><input class="form-control" type="text" autocomplete="off" required="" name="desc" value="<?php echo $id['profileDesc']; ?>"></div>
+        </div>
+        
+            <div class="col-md-8">
+                <h1>Editar perfil</h1>
+                <hr>
+                
+                <div class="row">
+                    <div class="col-sm-12 col-md-6">
+                        <div class="form-group mb-3"><label class="form-label">Nombre(s)</label><input id="firstname" class="form-control" type="text" name="fname" value="<?php echo $id['firstname_user'];?>"></div>
+                    </div>
+                    <div class="col-sm-12 col-md-6">
+                        <div class="form-group mb-3"><label class="form-label">Apellidos</label><input id="lastname" class="form-control" type="text" name="lName" value="<?php echo $id['lastname_user']; ?>"></div>
+                    </div>
+                </div>
+                
+                <div class="form-group mb-3"><label class="form-label">Email </label><input class="form-control" type="email" autocomplete="off" required="" name="mail" placeholder="" readonly value="<?php echo $id['mail_user']; ?>"> 
+                    <div class="form-group mb-3"><label class="form-label">Paypal Email</label><input class="form-control" type="email" autocomplete="off" name="paypal" value="<?php echo $id['paypal_info']; ?>">
+                    <div class="form-group mb-3"><label class="form-label">Fecha de nacimiento</label><input class="form-control" id="date" type="date" name="dateBt" value="<?php echo $id['dateBirth_user']; ?>"></div></div>
+                </div>
+                
+                </div>
+                    
+                <div class="row">
+                    <div class="col-md-12 content-right"><a href="profile.php">Volver al perfil</a>
+                        <input class="btn btn-primary text-end" value="Guardar Cambios" type="submit">
+                    </div>
+                </div>
+            </div>
+        </div>
+        </form>
+        <?php 
+        include('SQL/userCRUD.php');
+        $mysqli = new CRUDuser();
+        
+        if(isset($_POST['fName']) && isset($_POST['fName']) && isset($_POST['mail']) 
+            && isset($_POST['paypal']) && isset($_POST['birthDt']) && isset($_POST['desc'])
+            ){
+            $idU = $id['id_user'];
+            $fName=$_POST['fName'];
+            $lName=$_POST['lName'];
+            $mail=$_POST['mail'];
+            $paypal=$_POST['paypal'];
+            $birthDt=$_POST['birthDt'];
+            $desc=$_POST['desc'];
+            
+            $mysqli->update($idU, $fName, $lName, $mail, $paypal, $birthDt, $desc);
+        }
+    ?>
+    </div>
+    <p>Aquí puedes decidir si quieres eliminar tu cuenta.&nbsp;<a href="delete-account.php">Eliminar cuenta</a></p>
+    <?php else: ?>
+      <h1>Por favor ingrese su cuenta o registrese </h1>
+
+      <a href="login.php">Iniciar sesion</a> o
+      <a href="signup.php">Registro</a>
+    <?php endif; ?>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/js/Profile-Edit-Form.js"></script>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+  require 'session.php';
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Bienvenido</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+  </head>
+<body>
+    <?php if(!empty($id)): ?>
+
+      <section>
+        <article>
+          <h1>Bienvenido <?= $id['firstname_user']; ?>!</h1><br>
+          <p> Nombre: <?= $id['firstname_user']; ?></p><br>
+          <p> Appellido: <?= $id['lastname_user']; ?></p><br>
+          <p> Mail: <?= $id['mail_user']; ?></p><br>
+          <p> Paypal: <?= $id['paypal_info']; ?></p><br>
+          <p> Cumpleaños: <?= $id['dateBirth_user']; ?></p><br>
+          <p> Descripción: <?= $id['profileDesc']; ?></p><br>
+          <p> <a class="link" href="uploadvideo.php">Subir Video</a></p><br>
+          <p> <a class="link" href="edit-profile.php">Editar Perfil</a></p><br>
+          <p> <a class="link" href="logout.php">Logout</a></p><br>
+        </article>
+      </section>
+
+    <?php else: ?>
+      <h1>Por favor ingrese su cuenta o registrese </h1>
+
+      <a href="login.php">Iniciar sesion</a> o
+      <a href="signup.php">Registro</a>
+    <?php endif; ?>
+</body>
+</html>
