a68c64baef4b4c400039620f907537678a1f7559
gusvalladolid <81264925+gusvalladolid@users.noreply.github.com>
2021-11-25 16:21:27 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+    <title>StudyFans</title>
+</head>
+<body>
+
+  <div class="logo">
+    <form action="">
+      <h2>¿Who are we?</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed expedita quis enim reprehenderit maiores sunt delectus cumque, perspiciatis natus quo consectetur cupiditate! Voluptates ut sunt cum sit deserunt doloremque ad.</p>
+    <input type="submit" value="Registrate" class="logButton" />
+    <input type="submit" value="Iniciar Sesión" class="logButton" />
+    </form>
+  
+      <h3 class="logo">StudyFans</h3>
+    
+    </div>
+  
+
+
+    <form action="">
+    <div class="redes-sociales">
+      <h2>Our Social Network </h2>
+      <a href="https://twitter.com/GustavoVallado4" class="boton-redes twitter fab fa-twitter" target="_blank"><i class="icon-twitter"></i></a>
+      <p>@GustavoVallado4</p>
+    </div>
+    <div class="redes-sociales">
+      <a href="https://twitter.com/Aether_Fabian" class="boton-redes twitter fab fa-twitter" target="_blank"><i class="icon-twitter"></i></a>
+      <p>@Aether_Fabian</p>
+    </div>
+    <div class="redes-sociales">
+      <a href="https://twitter.com/Vornic_" class="boton-redes twitter fab fa-twitter" target="_blank"><i class="icon-twitter"></i></a>
+      <p>@Vornic_</p>
+    </div>
+    <div class="redes-sociales">
+      <a href="https://twitter.com/TurcoAv" class="boton-redes twitter fab fa-twitter" target="_blank"><i class="icon-twitter"></i></a>
+      <p>@TurvoAv</p>
+    </div>
+  
+</form>
+</form>
+</body>
+</html>
