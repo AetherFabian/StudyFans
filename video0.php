e06caef139ecffb0fb7f1e28425ab3451bb9fa7c
CarlosSSC <80538989+CarlosSSC@users.noreply.github.com>
2021-11-25 22:39:45 -0700

    Video para checar

---
+++ video0.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>2</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/html5-video.css">
+    <link rel="stylesheet" href="assets/css/Like-Button-for-PanelBear-Analytics.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                <h1>VIDEO TITLE</h1><video controls>
+  <source src="assets/videos/fileName.mp4" type="video/mp4">
+  Your browser does not support HTML5 video.
+</video><dl class="row">
+  <dt class="col-sm-3">Description lists</dt>
+  <dd class="col-sm-9">A description list is perfect for defining terms.</dd>
+</dl>
+            </div>
+            <div class="col-md-4"><div class="container">
+    <div class="row">
+        <div class="col-md-12">
+
+<div id="comments" class="comments-area">
+
+			<h2 class="comments-title">Comments</h2>
+
+		
+		<ol class="comment-list">
+					<li id="comment-1" class="comment even thread-even depth-1">
+			<article id="div-comment-1" class="comment-body">
+				<footer class="comment-meta">
+					<div class="comment-author vcard">						
+                        <b class="fn"> Sr. WordPress</b> <span class="says">Says:</span>					</div><!-- .comment-author -->
+
+					<div class="comment-metadata">
+						<a href="http://localhost/wordpress/2016/10/05/ola-mundo/#comment-1">
+							
+						</a>
+											</div><!-- .comment-metadata -->
+
+									</footer><!-- .comment-meta -->
+
+				<div class="comment-content">
+					<p>Olá, Isto é um comentário.<br>
+Para excluir um comentário, faça o login e veja os comentários de posts. Lá você terá a opção de editá-los ou excluí-los.</p>
+				</div><!-- .comment-content -->
+
+		</article><!-- .comment-body -->
+</li><!-- #comment-## -->
+		</ol><!-- .comment-list -->
+
+		
+					
+					</div><!-- #respond -->
+		
+            
+        </div>  
+        </div>
+    </div>
+</div>
+        </div>
+    </div>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+</body>
+
+</html>
