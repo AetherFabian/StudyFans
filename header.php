05670cc6210872bf46abb181d32fd2ef9e7203fe
gusvalladolid <81264925+gusvalladolid@users.noreply.github.com>
2021-11-25 17:19:47 -0700

    Add files via upload

---
+++ header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="header2.css">
+    <script src="https://kit.fontawesome.com/19fdbc0fa6.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <title>Document</title>
+</head>
+<body>
+  <input type="checkbox" id="check">
+   <nav>
+     <div class="row container-fluid">
+     <div class="icon col-sm-2 col-12">Study Fans</div>
+     <div class="search_box col-sm-2 col-12"> 
+       <input type="search" placeholder="Buscar">
+       <span class="fa fa-search"></span>
+     </div>
+     <div class="col-sm-2 col-12">
+     <ol class="list-inline text-right pt-2">
+       <li class="list-inline-item pt-auto"><a href="#"><i class="fas fa-upload"></i></a></li>
+       <li class="list-inline-item pt-auto"><a href="#"><i class="fas fa-user-circle"></i></a></li>
+     </ol>
+     <label for="check" class="bar" >
+       <span class="fa fa-bars" id="bars"></span>
+       <span class="fa fa-times" id="times"></span>
+     </label>
+     </div>
+     </div>
+   </nav>
+</body>
+</html>
