f1e584d38b6bf91c7c9dc511769488c1a0641d23
Joshua Avilés <80539008+JoshuaAv07@users.noreply.github.com>
2021-11-25 17:54:07 -0700

    header

---
+++ partials/header.php
@@ -0,0 +1,25 @@
+
+    <!--<meta name="viewport" content="width=device-width, initial-scale=1.0">-->
+    <link rel="stylesheet" href="CSS/header.css">
+    <script src="https://kit.fontawesome.com/19fdbc0fa6.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+  <input type="checkbox" id="check">
+   <nav>
+     <div class="row container-fluid">
+     <div class="icon col-sm-2 col-12"><a class="index" href="index.php">Study Fans</a></div>
+     <div class="search_box col-sm-2 col-12"> 
+       <input type="search" placeholder="Buscar">
+       <span class="fa fa-search"></span>
+     </div>
+     <div class="col-sm-2 col-12">
+     <ol class="list-inline text-right pt-2">
+       <li class="list-inline-item pt-auto"><a href="uploadvideo.php"><i class="fas fa-upload"></i></a></li>
+       <li class="list-inline-item pt-auto"><a href="profile.php"><i class="fas fa-user-circle"></i></a></li>
+     </ol>
+     <label for="check" class="bar" >
+       <span class="fa fa-bars" id="bars"></span>
+       <span class="fa fa-times" id="times"></span>
+     </label>
+     </div>
+     </div>
+   </nav>
